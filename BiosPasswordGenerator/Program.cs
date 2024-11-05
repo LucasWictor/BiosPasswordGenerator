0a180de0d97289119273a8ab840a466fedce20cb
LucasWictor <Dahlulle@gmail.com>
2024-11-05 20:46:59 +0100

    Added Asus as option for console app

--- BiosPasswordGenerator/Program.cs
+++ BiosPasswordGenerator/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using BiosPasswordGenerator.Vendors;
 
-Console.WriteLine("Hello, World!");
+namespace BiosPasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter year:");
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter month:");
+            int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter day:");
+            int day = int.Parse(Console.ReadLine());
+
+            string password = AsusGenerator.GeneratePassword(year, month, day);
+            Console.WriteLine($"Generated ASUS BIOS Password: {password}");
+        }
+    }
+}
